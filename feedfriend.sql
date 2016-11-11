363b011f05a476d85fd320dc3a23080a11bcd028
Chelsea Irizarry <cirizarr@mail.umw.edu>
2016-11-11 19:27:08 +0000

    database

---
+++ feedfriend.sql
@@ -0,0 +1,31 @@
+/*password = mealswipes*/
+CREATE EXTENSION pgcrypto;
+
+DROP DATABASE IF EXISTS feedfriend;
+CREATE DATABASE feedfriend;
+
+\c feedfriend
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+    id serial NOT NULL,
+    username text NOT NULL,
+    password text NOT NULL,
+    PRIMARY KEY (id)
+);
+
+DROP TABLE IF EXISTS profile;
+CREATE TABLE profile (
+    id serial NOT NULL,
+    name varchar(20) NOT NULL,
+    email varchar(15) NOT NULL,
+    image bytea,
+    PRIMARY KEY (id),
+    
+    userid serial NOT NULL,
+    CONSTRAINT users_userid_fk
+    FOREIGN KEY (userid)
+    REFERENCES users (id)
+);
+
+INSERT INTO users(username, password) VALUES('testuser', crypt('testpassword', gen_salt('bf')));
+INSERT INTO profile(name, email) VALUES('test', 'test@umw.edu');
