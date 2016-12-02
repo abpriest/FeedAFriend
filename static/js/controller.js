f89060dbf235baf8a5f0d44ab301fb2c4426d0b5
Nate Nuval <nnuval@mail.umw.edu>
2016-12-02 23:02:49 +0000

    search results populate

--- static/js/controller.js
+++ static/js/controller.js
@@ -7,33 +7,43 @@ App.controller('AppController', function($scope){
     $scope.found2 = [];
     
     socket.on('connect', function(){
-       console.log('Connected from controller'); 
+        console.log('Connected from controller'); 
     });
     
     $scope.search = function search() {
+        $scope.found2 = [];
+        //clear();
+        
         console.log("Searching for " + $scope.searchFor);
         socket.emit('sSearch', $scope.searchFor);
-        
+    
+        //$scope.$apply();
+    };
+    
+    $scope.clear = function clear(){
+        $scope.found2 = [];
+        $scope.$apply();
     };
     
     socket.on('found', function(res,srch_type){
-       $scope.found = res;
-       var i;
-       if ($scope.found[0].length > 2){
-           for (i =0; i< $scope.found.length; i++){
-            $scope.found2[i] = $scope.found[i][3] + " is available for " + $scope.found[i][0] + " from " + $scope.found[i][1] + " - " + $scope.found[i][2];
-        console.log("~ " + $scope.found[i][3] + " is available from " + $scope.found[i][1] + " - " + $scope.found[i][2]); 
+        
+        $scope.found = res;
+        var i;
     
-       }
-       } else {
-       
-       
-       for (i =0; i< $scope.found.length; i++){
-           $scope.found2[i] = "~ " + $scope.found[i][1];
-        console.log("~ " + $scope.found[i][1]); 
-    
-       }
-       }
-     });
+        if ($scope.found[0].length > 2){
+            for (i =0; i< $scope.found.length; i++){
+                $scope.found2[i] = $scope.found[i][3] + " is available for " + $scope.found[i][0] + " from " + $scope.found[i][1] + " - " + $scope.found[i][2];
+                console.log("~ " + $scope.found[i][3] + " is available from " + $scope.found[i][1] + " - " + $scope.found[i][2]); 
+            }
+        } else {
+            for (i =0; i< $scope.found.length; i++){
+                $scope.found2[i] = "~ " + $scope.found[i][1];
+                console.log("~ " + $scope.found[i][1]); 
+            }
+        }
+        
+        $scope.$apply();
+        
+    });
     
 });
